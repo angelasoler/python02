6049d8b485b5b8f8dc1ce7a67f8ce82b5afd4e05
angelasoler <manzanaembicicleta@gmail.com>
2025-02-22 18:14:22 -0300

    ex01

---
+++ ex01/load_csv.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from pandas import DataFrame
+
+
+def load(path: str) -> DataFrame:
+    if path.endswith(".csv") is False:
+        print("Most be .csv file extension!")
+        return None
+    try:
+        pd.options.display.max_rows = 1
+        data = pd.read_csv(path, index_col='country')
+        print(f"Loading dataset of dimensions {data.shape}")
+        return data
+    except Exception as e:
+        print(e)
+    return None
